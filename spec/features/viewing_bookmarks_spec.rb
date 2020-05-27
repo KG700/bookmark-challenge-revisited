5065a94c005f056dda18763ada3bcce4ae848245
Karla <karla.gardiner@gmail.com>
2020-05-27 12:47:59 +0100

    Refactor: created view with list of bookmarks and moved all logic into Bookmark class

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,8 +1,16 @@
 feature 'Viewing bookmarks' do
 
-  scenario 'visiting the index page' do
-    visit '/'
-    expect(page).to have_content "Bookmark Manager"
+  # As a user
+  # So I can quickly find URL I want to remember
+  # I would like to see a list of bookmarks
+  feature 'Viewing bookmarks' do
+    scenario 'A user can see bookmarks' do
+      visit '/bookmarks'
+
+      expect(page).to have_content "http://www.makersacademy.com"
+      expect(page).to have_content "http://www.destroyallsoftware.com"
+      expect(page).to have_content "http://www.google.com"
+    end
   end
 
 end
