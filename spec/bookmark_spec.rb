d6e74e2104bd3c46b20ecf445ccdde66b6d678ef
Karla <karla.gardiner@gmail.com>
2020-05-28 15:11:28 +0100

    Testing environment has been set up

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,6 +5,12 @@ describe Bookmark do
   describe '.all' do
 
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include "http://www.makersacademy.com"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
 ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+
+require_relative './setup_test_database'
 
 Capybara.app = BookmarkManager
 
@@ -21,5 +24,9 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:each) do
+    setup_test_database
+  end
 
 end
