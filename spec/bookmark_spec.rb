e8ede51fdd034a0a123dc03ae1ca9a4c013cd3e5
Karla <karla.gardiner@gmail.com>
2020-05-29 17:39:04 +0100

    Wrapped bookmarks into Bookmark models and added a title

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,4 +1,5 @@
 require 'bookmark'
+require 'database_helpers'
 
 describe Bookmark do
 
@@ -6,23 +7,29 @@ describe Bookmark do
     it 'returns all bookmarks' do
       connection = PG.connect(dbname: 'bookmark_test')
 
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+      bookmark = Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+      Bookmark.create(title: 'Destroy All Software', url: 'http://www.destroyallsoftware.com')
+      Bookmark.create(title: 'Google', url: 'http://www.google.com')
 
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include "http://www.makersacademy.com"
-      expect(bookmarks).to include "http://www.destroyallsoftware.com"
-      expect(bookmarks).to include "http://www.google.com"
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers Academy'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
   end
 
   describe '.create' do
     it 'creates a new bookmark' do
-      Bookmark.create(url: 'http://www.testbookmark.com')
+      bookmark = Bookmark.create(title: 'Test Bookmark', url: 'http://www.testbookmark.com')
+      persisted_data = persisted_data(id: bookmark.id)
 
-      expect(Bookmark.all).to include 'http://www.testbookmark.com'
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data['id']
+      expect(bookmark.title).to eq 'Test Bookmark'
+      expect(bookmark.url).to eq 'http://www.testbookmark.com'
     end
   end
 
--- spec/features/bookmarks_user_stories_spec.rb
+++ spec/features/bookmarks_user_stories_spec.rb
@@ -1,21 +1,19 @@
-feature 'bookmarks' do
+feature 'User Stories: Bookmarks' do
 
   # As a user
   # So I can quickly find URL I want to remember
   # I would like to see a list of bookmarks
   feature 'Viewing bookmarks' do
     scenario 'A user can see bookmarks' do
-      connection = PG.connect(dbname: 'bookmark_test')
-
-      connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+      Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+      Bookmark.create(title: 'Destroy All Software', url: 'http://www.destroyallsoftware.com')
+      Bookmark.create(title: 'Google', url: 'http://www.google.com')
 
       visit '/bookmarks'
 
-      expect(page).to have_content "http://www.makersacademy.com"
-      expect(page).to have_content "http://www.destroyallsoftware.com"
-      expect(page).to have_content "http://www.google.com"
+      expect(page).to have_link 'Makers Academy', href: "http://www.makersacademy.com"
+      expect(page).to have_link 'Destroy All Software', href: "http://www.destroyallsoftware.com"
+      expect(page).to have_link 'Google', href: "http://www.google.com"
     end
   end
 
@@ -26,9 +24,10 @@ feature 'bookmarks' do
     scenario do
       visit '/bookmarks/new'
       fill_in :url, with: 'http://www.testbookmark.com'
+      fill_in :title, with: 'Test Bookmark'
       click_button 'Submit'
 
-      expect(page).to have_content 'http://www.testbookmark.com'
+      expect(page).to have_link 'Test Bookmark', href: 'http://www.testbookmark.com'
     end
   end
 
