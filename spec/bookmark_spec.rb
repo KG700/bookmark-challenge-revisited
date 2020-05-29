ba7aa9236f5b9f854ad96901191b8ed42a895188
Karla <karla.gardiner@gmail.com>
2020-05-29 15:50:11 +0100

    Moved create method into Bookmark model

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,7 +3,6 @@ require 'bookmark'
 describe Bookmark do
 
   describe '.all' do
-
     it 'returns all bookmarks' do
       connection = PG.connect(dbname: 'bookmark_test')
 
@@ -17,7 +16,14 @@ describe Bookmark do
       expect(bookmarks).to include "http://www.destroyallsoftware.com"
       expect(bookmarks).to include "http://www.google.com"
     end
+  end
 
+  describe '.create' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.testbookmark.com')
+
+      expect(Bookmark.all).to include 'http://www.testbookmark.com'
+    end
   end
 
 end
